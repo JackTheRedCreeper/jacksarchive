9a596fb500c6452e02251eccf2bd68536837447a
unknown <12295605+JackTheRedCreeper@users.noreply.github.com>
2024-04-03 05:31:51 +0200

    Quartz sync: Apr 3, 2024, 5:24 AM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -16,25 +16,18 @@ export default (() => {
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
     const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://i.imgur.com/qNlllmX.png`
+    const ogImagePath = "static/og-image.png"
 
     return (
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
-          </>
-        )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
+        <meta property="og:width" content="400" />
+        <meta property="og:height" content="269" />
         <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
